466492a6887db9bfb1a0e07c27e00e8ff57ee5f8
Kevin Deldycke <kevin@deldycke.com>
2011-09-29 16:10:48 +0000

    Update the project structure to include cells.

--- smile_matrix/smile_project.py
+++ smile_matrix/smile_project.py
@@ -24,8 +24,11 @@ import datetime
 from osv import osv, fields
 
 
+
 class smile_project(osv.osv):
     _name = 'smile.project'
+
+    _order = "start_date"
 
     _columns = {
         'name': fields.char('Name', size=32),
@@ -39,43 +42,93 @@ class smile_project(osv.osv):
         'end_date': (datetime.datetime.today() + datetime.timedelta(days=7)).strftime('%Y-%m-%d'),
         }
 
+
+    ## Constraints
+
+    def _check_date_range(self, cr, uid, ids, context=None):
+        for project in self.browse(cr, uid, ids, context):
+            start_date = datetime.datetime.strptime(project.start_date, '%Y-%m-%d')
+            end_date = datetime.datetime.strptime(project.end_date, '%Y-%m-%d')
+            if end_date < start_date:
+                return False
+        return True
+
+    _constraints = [
+        (_check_date_range, "Start date can't be greater than end date.", ['start_date', 'end_date'])
+        ]
+
+
+    ## Native methods
+
+    def write(self, cr, uid, ids, vals, context=None):
+        ret = super(smile_project, self).write(cr, uid, ids, vals, context)
+        # Automaticcaly remove out of range cells if dates changes
+        if 'start_date' in vals or 'end_date' in vals:
+            self.remove_outdated_cells(cr, uid, ids, vals, context)
+        return ret
+
+
+    ## Custom methods
+
+    def get_date_range(self, start_date, end_date, day_delta=1):
+        """ Get a list of date objects covering the given date range
+        """
+        date_range = []
+        if not isinstance(start_date, (datetime.date, datetime.datetime)):
+            start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        if not isinstance(end_date, (datetime.date, datetime.datetime)):
+            end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+        date = start_date
+        while date <= end_date:
+            date_range.append(date)
+            date = date + datetime.timedelta(days=day_delta)
+        return date_range
+
+    def remove_outdated_cells(self, cr, uid, ids, vals, context):
+        """ This method remove out of range cells on each sub lines
+        """
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        outdated_cells = []
+        for project in self.browse(cr, uid, ids, context):
+            start_date = datetime.datetime.strptime(project.start_date, '%Y-%m-%d')
+            end_date = datetime.datetime.strptime(project.end_date, '%Y-%m-%d')
+            for line in project.line_ids:
+                for cell in line.cell_ids:
+                    date = datetime.datetime.strptime(cell.date, '%Y-%m-%d')
+                    if date < start_date or date > end_date:
+                        # Cell is out of range. Delete it.
+                        outdated_cells.append(cell.id)
+
+        if outdated_cells:
+            self.pool.get('smile.project.line.cell').unlink(cr, uid, outdated_cells, context)
+        return
+
     def matrix(self, cr, uid, ids, context=None):
-        if len(ids) >1:
+        if len(ids) > 1:
             raise osv.except_osv('Error', 'len(ids) !=1')
         project = self.browse(cr, uid, ids[0], context)
-
         vals = {}
         for month in self.pool.get('smile.matrix')._get_project_months(project):
             month_str = self.pool.get('smile.matrix')._month_to_str(month)
             for line in project.line_ids:
                 vals['cell_%s_%s' % (line.id, month_str)] = line.price
-
         new_context = context.copy()
         new_context['project_id'] = ids[0]
         matrix_id = self.pool.get('smile.matrix').create(cr, uid, vals, new_context)
         return {
-                'name': "%s matrix" % (project.name,),
-                'type': 'ir.actions.act_window',
-                'res_model': 'smile.matrix',
-                'res_id': matrix_id,
-                'view_mode': 'form',
-                'view_type': 'form',
-                'context': new_context,
-                'target': 'new',
-                }
-
-    def _check_months(self, cr, uid, ids):
-        for project in self.browse(cr, uid, ids):
-            start = datetime.datetime.strptime(project.start_date, '%Y-%m-%d')
-            end = datetime.datetime.strptime(project.end_date, '%Y-%m-%d')
-            if end < start:
-                return False
-        return True
-
-    _constraints = [(_check_months, "End < start", ['start_date', 'end_date'])]
-
+            'name': "%s matrix" % (project.name,),
+            'type': 'ir.actions.act_window',
+            'res_model': 'smile.matrix',
+            'res_id': matrix_id,
+            'view_mode': 'form',
+            'view_type': 'form',
+            'context': new_context,
+            'target': 'new',
+            }
 
 smile_project()
+
 
 
 class smile_project_line(osv.osv):
@@ -85,6 +138,84 @@ class smile_project_line(osv.osv):
         'name': fields.char('Name', size=32, required=True),
         'price': fields.float('Price'),
         'project_id': fields.many2one('smile.project', "Project", required=True, ondelete='cascade'),
+        'cell_ids': fields.one2many('smile.project.line.cell', 'line_id', "Cells"),
         }
 
+
+    ## Native methods
+
+    def create(self, cr, uid, vals, context=None):
+        line_id = super(smile_project_line, self).create(cr, uid, vals, context)
+        # Create default cells
+        line = self.browse(cr, uid, line_id, context)
+        self.generate_cells(cr, uid, line, context)
+        return line_id
+
+
+    ## Custom methods
+
+    def generate_cells(self, cr, uid, line, context=None):
+        """ This method generate all cells between the date range.
+        """
+        project = line.project_id
+        date_range = self.pool.get('smile.project').get_date_range(project.start_date, project.end_date)
+        vals = {
+            'line_id': line.id
+            }
+        for date in date_range:
+            vals.update({'date': date})
+            self.pool.get('smile.project.line.cell').create(cr, uid, vals, context)
+        return
+
+
 smile_project_line()
+
+
+
+class smile_project_line_cell(osv.osv):
+    _name = 'smile.project.line.cell'
+
+    _order = "date"
+
+    _columns = {
+        'date': fields.date('Date', required=True),
+        'quantity': fields.float('Quantity', required=True),
+        'line_id': fields.many2one('smile.project.line', "Project line", required=True, ondelete='cascade'),
+        }
+
+    _defaults = {
+        'quantity': 0.0,
+        }
+
+
+    ## Constraints
+
+    def _check_quantity(self, cr, uid, ids, context=None):
+        for cell in self.browse(cr, uid, ids, context):
+            if cell.quantity < 0:
+                return False
+        return True
+
+    def _check_date(self, cr, uid, ids, context=None):
+        for cell in self.browse(cr, uid, ids,context):
+            date = datetime.datetime.strptime(cell.date, '%Y-%m-%d')
+            project = cell.line_id.project_id
+            start_date = datetime.datetime.strptime(project.start_date, '%Y-%m-%d')
+            end_date = datetime.datetime.strptime(project.end_date, '%Y-%m-%d')
+            if date < start_date or date > end_date:
+                return False
+        return True
+
+    def _check_duplicate(self, cr, uid, ids, context=None):
+        for cell in self.browse(cr, uid, ids, context):
+            if len(self.search(cr, uid, [('date', '=', cell.date), ('line_id', '=', cell.line_id.id)], context=context)) > 1 :
+                return False
+        return True
+
+    _constraints = [
+        (_check_quantity, "Quantity can't be negative.", ['quantity']),
+        (_check_date, "Cell date is out of the project date range.", ['date']),
+        (_check_duplicate, "Two cells can't share the same date.", ['date']),
+        ]
+
+smile_project_line_cell()
