16341bfbdfe9617a65312a362afb11f55941b8f5
Corentin POUHET-BRUNERIE <copou@smile.fr>
2011-10-15 00:54:13 +0200

    [IMP] Add _deactivate_old_forecast_lines method

--- smile_analytic_forecasting/analytic.py
+++ smile_analytic_forecasting/analytic.py
@@ -86,6 +86,14 @@ AnalyticPeriod()
 class AnalyticLine(osv.osv):
     _inherit = 'account.analytic.line'
 
+    def __init__(self, pool, cr):
+        super(AnalyticLine, self).__init__(pool, cr)
+        self._unicity_fields = [field for field in self._columns \
+            if self._columns[field]._type not in ('one2many', 'many2many') \
+            and field not in ('name', 'code', 'ref', 'date', 'period_id', \
+                              'amount', 'unit_amount', 'amount_currency', \
+                              'currency_id', 'move_id')]
+
     def _get_period_id(self, cr, uid, ids, name, arg, context=None):
         if isinstance(ids, (int, long)):
             ids = [ids]
@@ -96,12 +104,12 @@ class AnalyticLine(osv.osv):
         return res
 
     _columns = {
-        'active': fields.boolean('Active'),
         'type': fields.selection([('actual', 'Actual'), ('forecast', 'Forecast')], 'Type', required=True),
         'period_id': fields.many2one('account.analytic.period', 'Period', domain=[('state', '!=', 'done')]),
         'analysis_period_id': fields.function(_get_period_id, method=True, type='many2one', relation='account.analytic.period', string='Analysis Period', store={
             'account.analytic.line': (lambda self, cr, uid, ids, context=None: ids, ['date'], 10),
         }),
+        'active': fields.boolean('Active'),
     }
 
     def _get_default_period_id(self, cr, uid, context=None):
@@ -141,4 +149,48 @@ class AnalyticLine(osv.osv):
         (_check_actual_from_analysis_period, 'You cannot pass an actual entry in a future period!', ['type', 'period']),
         (_check_forecast_from_analysis_period, 'You cannot pass a forecast entry in a past period!', ['type', 'period']),
     ]
+
+    def _deactivate_old_forecast_lines(self, cr, uid, ids, context=None):
+        line_ids_to_deactivate = []
+        context = context or {}
+        unicity_fields = self._unicity_fields
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        for line in self.read(cr, uid, ids, unicity_fields, {}):
+            domain = []
+            if context.get('ids_to_exclude'):
+                domain.append(('id', 'not in', context['ids_to_exclude']))
+            for field in unicity_fields:
+                value = isinstance(line[field], tuple) and line[field][0] or line[field]
+                domain.append((field, '=', value))
+            key_line_ids = self.search(cr, uid, domain, order='period_id desc, type asc', context={'active_test': True})
+            no_actual_lines = 1
+            for index, key_line in enumerate(self.read(cr, uid, key_line_ids, ['period_id', 'type'], context)):
+                if key_line['type'] == 'actual':
+                    no_actual_lines = 0
+                if key_line['type'] == 'forecast':
+                    line_ids_to_deactivate.extend(key_line_ids[index + no_actual_lines:])
+                    break
+        return self.write(cr, uid, line_ids_to_deactivate, {'active': False}, context)
+
+    def create(self, cr, uid, vals, context=None):
+        res_id = super(AnalyticLine, self).create(cr, uid, vals, context)
+        self._deactivate_old_forecast_lines(cr, uid, res_id, context)
+        return res_id
+
+    def write(self, cr, uid, ids, vals, context=None):
+        res = super(AnalyticLine, self).write(cr, uid, ids, vals, context)
+        unicity_fields = self._unicity_fields + ['period_id', 'type']
+        for field in vals:
+            if field in unicity_fields:
+                self._deactivate_old_forecast_lines(cr, uid, ids, context)
+                break
+        return res
+
+    def unlink(self, cr, uid, ids, context=None):
+        context = context or {}
+        context['ids_to_exclude'] = isinstance(ids, (int, long)) and [ids] or ids
+        res = super(AnalyticLine, self).unlink(cr, uid, ids, context)
+        self._deactivate_old_forecast_lines(cr, uid, ids, context)
+        return res
 AnalyticLine()
