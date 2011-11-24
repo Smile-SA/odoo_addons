fca838c2b32c1bf6596e8deeb029d0f4d8a4c231
Corentin POUHET-BRUNERIE <copou@smile.fr>
2011-11-24 15:19:04 +0100

    [FIX] Correct several bugs in smile_access_control

--- smile_access_control/res_user.py
+++ smile_access_control/res_user.py
@@ -29,7 +29,8 @@ class ResUser(osv.osv):
         'user_profile_id': fields.many2one('res.users', 'User Profile', domain=[('user_profile', '=', True)]),
         'user_ids': fields.one2many('res.users', 'user_profile_id', 'Users', domain=[('user_profile', '=', False)]),
         'field_ids': fields.many2many('ir.model.fields', 'res_users_fields_rel', 'user_id', 'field_id', 'Fields to update',
-                                      domain=[('model', '=', 'res.users')]),
+                                      domain=[('model', '=', 'res.users'),
+                                              ('name', 'not in', ('user_profile', 'user_profile_id', 'user_ids', 'field_ids'))]),
     }
 
     def _get_default_field_ids(self, cr, uid, ids, context=None):
@@ -44,14 +45,20 @@ class ResUser(osv.osv):
 
     def onchange_user_profile(self, cr, uid, ids, user_profile):
         if user_profile:
-            return {'value': {'active': False, 'user_profile_id': False}} 
+            return {'value': {'active': ids == [1] and True or False, 'user_profile_id': False}}
         return {}
 
     def _get_user_vals_from_profile(self, cr, uid, user_profile_id, context=None):
         vals = {}
-        user_profile = self.browse(cr, uid, user_profile_id, context)
-        for field in user_profile.field_ids:
-            vals[field.name] = user_profile[field.name]
+        user_profile = self.read(cr, uid, user_profile_id)
+        for field in self.pool.get('ir.model.fields').read(cr, uid, user_profile['field_ids'], ['name']):
+            value = user_profile[field['name']]
+            if isinstance(value, tuple):
+                value = value[0]
+            if isinstance(value, list):
+                value = [(6, 0, value)]
+            vals[field['name']] = value
+        vals['user_ids'] = [(5, 0)]
         return vals
 
     def create(self, cr, uid, vals, context=None):
@@ -60,7 +67,13 @@ class ResUser(osv.osv):
         return super(ResUser, self).create(cr, uid, vals, context)
 
     def write(self, cr, uid, ids, vals, context=None):
+        if isinstance(ids, (int, long)):
+            ids = [ids]
         if vals.get('user_profile_id'):
             vals.update(self._get_user_vals_from_profile(cr, uid, vals['user_profile_id'], context))
+        else:
+            for user_profile in self.read(cr, uid, ids, ['user_profile', 'user_ids'], context):
+                if user_profile['user_profile'] and user_profile['user_ids']:
+                    self.write(cr, uid, user_profile['user_ids'], {'user_profile_id': user_profile['id']}, context)
         return super(ResUser, self).write(cr, uid, ids, vals, context)
 ResUser()
