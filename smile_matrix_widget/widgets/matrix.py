1a531cf10a1120f18b3fe96aa53aad2555cddc38
Kevin Deldycke <kevin@deldycke.com>
2011-12-15 10:22:18 +0000

    Save context and attrs experiments.

--- smile_matrix_widget/widgets/matrix.py
+++ smile_matrix_widget/widgets/matrix.py
@@ -34,12 +34,12 @@ class Matrix(TinyInputWidget):
         JSLink("smile_matrix_widget", "javascript/matrix.js"),
         ]
 
-    params = ['widget']
 
-    def __init__(self, **attrs):
-        super(Matrix, self).__init__(**attrs)
+    def __init__(self, name, model, view=None, domain=[], context={}, **kwargs):
+        #attrs = kwargs.get('attrs', {})
+        #xml_attrs   = isinstance(attrs,   (str, unicode)) and eval(attrs)   or attrs
+        #xml_context = isinstance(context, (str, unicode)) and eval(context) or context
+        super(Matrix, self).__init__(**kwargs)
 
-    def set_value(self, value):
-        self.default = value
 
 register_widget(Matrix, ["matrix"])
