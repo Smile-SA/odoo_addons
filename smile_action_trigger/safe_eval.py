1236a63582315acf965a2146e47a5648412808b9
Corentin POUHET-BRUNERIE <corentin.pouhet-brunerie@smile.fr>
2012-01-05 19:07:01 +0100

    [IMP] Override safe_eval in order to complete itds globals_dict with Python builtins

--- smile_action_trigger/safe_eval.py
+++ smile_action_trigger/safe_eval.py
@@ -19,6 +19,34 @@
 #
 ##############################################################################
 
-import safe_eval
-import ir_actions
-import sartre
+"""
+Overriding eval function defined in the base module
+in order to complete globals_dict with missing Python __builtins__
+"""
+
+from base.ir import ir_actions
+
+native_safe_eval = ir_actions.eval
+
+def new_safe_eval(expr, globals_dict=None, locals_dict=None, mode="eval", nocopy=False):
+    globals_dict = globals_dict or {}
+    globals_dict.update({
+                'divmod': divmod,
+                'enumerate': enumerate,
+                'float': float,
+                'int': int,
+                'isinstance': isinstance,
+                'max': max,
+                'min': min,
+                'pow': pow,
+                'range': range,
+                'reversed': reversed,
+                'sorted': sorted,
+                'sum': sum,
+                'type': type,
+                'xrange': xrange,
+                'zip': zip,
+    })
+    return native_safe_eval(expr, globals_dict, locals_dict, mode, nocopy)
+
+ir_actions.eval = new_safe_eval
