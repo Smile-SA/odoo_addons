c2511677d8521e82ed7e1a1355d91d50bf4b1e28
Kevin Deldycke <kevin@deldycke.com>
2012-01-27 15:31:00 +0000

    Restrict custom color effect to matrix elements.

--- smile_matrix_demo/smile_activity.py
+++ smile_matrix_demo/smile_activity.py
@@ -95,7 +95,7 @@ class smile_activity_report(osv.osv):
                 $(document).ready(function(){
                     /* Highlight in blue grand total of all matrix every 2 seconds */
                     setInterval(function(){
-                        $("td[id$='__grand_total']").effect("highlight", {'color': '#00f'}, 1000);
+                        $(".matrix td[id$='__grand_total']").effect("highlight", {'color': '#00f'}, 1000);
                     }, 2 * 1000);
                 });
                 """,
