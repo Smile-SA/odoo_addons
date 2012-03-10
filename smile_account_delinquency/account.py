5c9470418f5eed98e487dd14141998c286e8e70f
Corentin POUHET-BRUNERIE <copou@BURLINGTON.(none)>
2012-03-10 11:50:54 +0100

    [ADD] Add smile_account_delinquency addon

---
+++ smile_account_delinquency/account.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011-2012 Smile (<http://www.smile.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+
+import time
+
+from osv import osv, fields
+
+class AccountMove(osv.osv):
+    _inherit = 'account.move'
+
+    _columns = {
+        'reversed': fields.boolean('Reversed', readonly=True),
+    }
+
+    def create_reversal(self, cr, uid, ids, reversal_date, reversal_ref_prefix=False, reversal_line_prefix=False, reconcile=True, context=None):
+        self.write(cr, uid, ids, {'reversed': True}, context)
+        reversal_ref_prefix = reversal_ref_prefix or 'REV-'
+        return super(AccountMove, self).create_reversal(cr, uid, ids, reversal_date, reversal_ref_prefix, reversal_line_prefix, reconcile, context)
+
+    def button_cancel(self, cr, uid, ids, context=None):
+        move_ids_to_reverse = []
+        move_ids_not_to_reverse = []
+        for move in self.browse(cr, uid, ids, context=context):
+            if not move.journal_id.update_posted:
+                move_ids_to_reverse.append(move.id)
+            else:
+                move_ids_not_to_reverse.append(move.id)
+        if move_ids_to_reverse:
+            context = context or {}
+            reversal_date = context.get('reversal_date') or time.strftime('%Y-%m-%d')
+            reversed_move_ids = self.create_reversal(cr, uid, move_ids_to_reverse, reversal_date)
+            self.button_validate(cr, uid, reversed_move_ids, context)
+        if move_ids_not_to_reverse:
+            super(AccountMove, self).button_cancel(cr, uid, move_ids_not_to_reverse, context)
+        return True
+
+    def unlink(self, cr, uid, ids, context=None, check=True):
+        move_ids_to_unlink = []
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        for move in self.read(cr, uid, ids, ['reversed'], context):
+            if not move['reversed']:
+                move_ids_to_unlink.append(move['id'])
+        return super(AccountMove, self).unlink(cr, uid, move_ids_to_unlink, context, check)
+AccountMove()
---
+++ smile_account_delinquency/account_voucher.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011-2012 Smile (<http://www.smile.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+
+from osv import osv
+from tools.translate import _
+
+class AccountVoucher(osv.osv):
+    _inherit = 'account.voucher'
+
+    def button_open_voucher_delinquency_wizard(self, cr, uid, ids, context=None):
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        context = context or {}
+        context['account_voucher_ids'] = ids
+        return {
+            'name': _('Unreconcile'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': False,
+            'res_model': 'account.voucher.reversal',
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'context': context
+        }
+AccountVoucher()
+
