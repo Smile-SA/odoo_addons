f6e8e9b34326bead383116e6a363bcebb9969c04
Kevin Deldycke <kevin@deldycke.com>
2012-04-02 07:49:57 +0000

    Don't flash the grand total continuously: it gets annoying really fast.

--- smile_matrix_demo/custom.js
+++ smile_matrix_demo/custom.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-    /* Highlight in blue grand total of all matrix every 2 seconds */
-    setInterval(function(){
+    /* Highlight the grand total of all matrix in blue every 2 seconds, but stop after 3 times */
+    var counter = 0;
+    var interval_id = setInterval(function(){
         $(".matrix td[id$='__grand_total']").effect("highlight", {'color': '#00f'}, 1000);
+        if (++counter === 3) {
+            window.clearInterval(interval_id);
+        };
     }, 2 * 1000);
 });
