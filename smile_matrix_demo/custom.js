c56d3cfeef9295f50e526e4ae7a305fb1318d7ff
Kevin Deldycke <kevin@deldycke.com>
2012-04-02 16:03:00 +0000

    Demonstrate how to add a custom header to the top of the matrix.

--- smile_matrix_demo/custom.js
+++ smile_matrix_demo/custom.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    /* Highlight the grand total of all matrix in blue every 2 seconds, but stop after 3 times */
+
+    // Highlight the grand total of all matrix in blue every 2 seconds, but stop after 3 times
     var counter = 0;
     var interval_id = setInterval(function(){
         $(".matrix td[id$='__grand_total']").effect("highlight", {'color': '#00f'}, 1000);
@@ -7,4 +8,10 @@ $(document).ready(function(){
             window.clearInterval(interval_id);
         };
     }, 2 * 1000);
+
+    // Add a top matrix header
+    $(".matrix thead tr:first").each(function(){
+        $(this).before('<tr><th colspan="' + $(this).children().length + '">Big matrix header</th><tr>');
+    });
+
 });
