f527fd85f1346381f59d3c2cb8ef76f524ba8ba7
xavfernandez <xav.fernandez@gmail.com>
2012-06-25 11:11:34 +0200

    Update plot_usage_graph script to use gc_browse_records_count

--- smile_server_monitoring/tools/plot_usage_graph.py
+++ smile_server_monitoring/tools/plot_usage_graph.py
@@ -1,9 +1,24 @@
+"""
+Get data with a cron:
+python your_path/smile_server_monitoring/tools/plot_usage_graph.py /home/openerp/graph.log
+or:
+python your_path/smile_server_monitoring/tools/plot_usage_graph.py --browse_records /home/openerp/graph.log
+
+Get the data in tabular form with:
+python your_path/smile_server_monitoring/tools/plot_usage_graph.py --regroup_infos /home/openerp/graph.log
+"""
+
+
 import xmlrpclib
 import time
 
-def write_infos_to_file(filename, limit=0, floor=10):
+def write_infos_to_file(filename, data_type='objects', limit=0, floor=10):
     sock_common = xmlrpclib.ServerProxy('http://localhost:8069/xmlrpc/common')
-    objects_count = sock_common.gc_types_count(limit, floor)
+    objects_count = []
+    if data_type == 'objects':
+        objects_count = sock_common.gc_types_count(limit, floor)
+    elif data_type == 'browse_records':
+        objects_count = sock_common.gc_browse_records_count(limit, floor)
     mem_usage = sock_common.get_memory()
     if mem_usage == 'Unknown':
         mem_usage = 0
@@ -11,8 +26,8 @@ def write_infos_to_file(filename, limit=0, floor=10):
         mem_usage = int(mem_usage[:-3])
     objects_count.insert(0, ('memory', mem_usage))
     point = (time.strftime('%Y-%m-%d %H:%M:%S'), objects_count)
-    with open(filename,'a') as log:
-        log.write(repr(point)+'\n')
+    with open(filename, 'a') as log:
+        log.write(repr(point) + '\n')
 
 def load_data_from_file(filename):
     """ Builds points=[
@@ -20,7 +35,7 @@ def load_data_from_file(filename):
                 (time2, [(obj1, count1b), (obj3, count3)],
                 ] from infos stored in filename with the same structure (stored via write_infos_to_file function)"""
     points = []
-    with open(filename,'r') as log:
+    with open(filename, 'r') as log:
         for line in log:
             points.append(eval(line))
     return points
@@ -38,7 +53,7 @@ def has_change(time_counts, min_change=0):
     for time, count in time_counts:
         if not first_count:
             first_count = count
-        if abs(first_count - count)>= min_change:
+        if abs(first_count - count) >= min_change:
             return True
     return False
 
@@ -58,7 +73,7 @@ def print_matrix(object_infos):
             timestamps.add(timestamp)
     timestamps = list(timestamps)
     timestamps.sort()
-    result = "\t".join(['time']+obj_types) + '\n'
+    result = "\t".join(['time'] + obj_types) + '\n'
     for curr_timestamp in timestamps:
         line_infos = [curr_timestamp]
         for obj, time_count in object_infos.items():
@@ -89,19 +104,24 @@ if __name__ == '__main__':
     import sys
     import pprint
 
+    usage = """ Usage: python plot_usage_graph.py filename [--browse_records] [--regroup_infos]"""
+
     if len(sys.argv) <= 1:
-    	print """ Usage: python plot_usage_graph.py filename [--regroup_infos]"""
+    	print usage
         exit()
     filename = sys.argv[1]
     if len(sys.argv) == 2:
         write_infos_to_file(filename)
+    elif sys.argv[2] == '--browse_records':
+        write_infos_to_file(filename, 'browse_records')
     elif sys.argv[2] == '--regroup_infos':
         data = load_data_from_file(filename)
         object_infos = regroup_data_per_object(data)
         object_infos = filter_unchanging_objects(object_infos, min_change=20)
         #pprint.pprint(object_infos)
         print_matrix(object_infos)
+
     else:
-    	print """ Usage: python plot_usage_graph.py filename [--regroup_infos]"""
+    	print usage
         exit()
 
