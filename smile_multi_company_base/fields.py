b294ea45f794d2a230e95d37fded38b63f43c505
Corentin PB <corentin.pb@gmail.com>
2012-07-24 09:12:23 +0200

    [IMP] Add French translations

--- smile_multi_company_base/fields.py
+++ smile_multi_company_base/fields.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011-2012 Smile (<http://www.smile.fr>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
