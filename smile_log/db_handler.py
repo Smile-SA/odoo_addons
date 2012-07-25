ec17d2c632ccb59d50c243575c0e1693f9f53871
Corentin PB <corentin.pb@gmail.com>
2012-07-25 18:48:14 +0200

    [FIX] Replace split(': ') to split(':')

--- smile_log/db_handler.py
+++ smile_log/db_handler.py
@@ -80,7 +80,7 @@ class SmileDBHandler(logging.Handler):
 def add_timing(original_method):
     def new_method(self, msg):
         delay = datetime.datetime.now() - self._logger_start
-        msg += " after %sh %smin %ss" % tuple(str(delay).split(': '))
+        msg += " after %sh %smin %ss" % tuple(str(delay).split(':'))
         return original_method(self, msg)
     return new_method
 
