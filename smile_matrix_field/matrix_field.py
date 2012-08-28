b44b44a57c6651474e7979593fdf3840677b51c4
Kevin Deldycke <kevin@deldycke.com>
2012-08-28 10:31:38 +0200

    Update copyright years.

--- smile_matrix_field/matrix_field.py
+++ smile_matrix_field/matrix_field.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2011 Smile. All Rights Reserved
+#    Copyright (C) 2011-2012 Smile. All Rights Reserved
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
