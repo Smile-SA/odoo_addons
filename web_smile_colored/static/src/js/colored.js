68d0ca3402b31d7c123757f963678b2468674767
xafer <xafer@portland.(none)>
2012-11-05 15:28:45 +0100

    [IMP] make it better work with 6.1 version (previous commits were in fact intended for version 7.0)

--- web_smile_colored/static/src/js/colored.js
+++ web_smile_colored/static/src/js/colored.js
@@ -1,6 +1,6 @@
-openerp.web_smile_colored = function (instance) {
+openerp.web_smile_colored = function (openerp) {
 
-  instance.web.form.FieldColored = instance.web.form.FieldFloat.extend({
+  openerp.web.form.FieldColored = openerp.web.form.FieldFloat.extend({
 
     set_value: function(value_) {
         this._super.apply(this, [value_]);
@@ -13,62 +13,90 @@ openerp.web_smile_colored = function (instance) {
     },
     change_color: function(value_) {
       var colored = JSON.parse(this.node.attrs.modifiers || '{}').colored || "{}";
+      var input = this.$element.find('input');
       if (value_) {
           for (var color in colored)
           {
           	var expr = colored[color];
           	expr_bool = eval(value_ + expr);
           	if (expr_bool == true) {
-          		this.$el.find('span').attr('style', "color:"+color)
+          		input.attr('style', "color:"+color+"; width: 100%");
           		return;
           	}
           }
         }
-        this.$el.find('span').removeAttr('style');
+      if (input.style) {
+      	input.style.color = '';
+      }
     },
 
   });
 
-  instance.web.form.widgets.add('colored', 'instance.web.form.FieldColored');
-
+  openerp.web.form.widgets.add('colored', 'openerp.web.form.FieldColored');
+  
   //########################################################
+  
+  openerp.web.page.FieldColoredReadonly = openerp.web.page.FieldFloatReadonly.extend({
+    
 
-  instance.web.list.ColoredColumn = instance.web.list.Column.extend({
-    init: function (id, tag, attrs) {
-        _.extend(attrs, {
-            id: id,
-            tag: tag
-        });
-
-        this.modifiers = attrs.modifiers ? JSON.parse(attrs.modifiers) : {};
-        delete attrs.modifiers;
-        _.extend(this, attrs);
-
-        if (this.modifiers['tree_invisible']) {
-            this.invisible = '1';
-        } else { delete this.invisible; }
-
-        if (this.modifiers['colored']) {
-            this.colored = this.modifiers['colored'];
-        } else { this.colored = {}; }
-    },
-
-    _format: function (row_data, options) {
-        if (row_data[this.id].value) {
-          for (var color in this.colored)
+    set_value: function(value) {
+      var colored = JSON.parse(this.node.attrs.modifiers || '{}').colored || "{}";
+      var div = this.$element.find('div');
+      if (value) {
+          for (var color in colored)
           {
-          	var expr = this.colored[color];
-          	expr_bool = eval(row_data[this.id].value + expr);
+          	var expr = colored[color];
+          	expr_bool = eval(value + expr);
           	if (expr_bool == true) {
-          		return _.str.sprintf('<span style="color:%s">%s</span>', color, row_data[this.id].value);
+          		if (!div.style) {
+          			div.attr('style', 'color:' + color + "; width: 100%");
+          		}
+          		else {
+          			div.style.color=color;
+          		}
+          		return this._super(value, this, '');
           	}
           }
         }
-        return _.escape(instance.web.format_value(
-            row_data[this.id].value, this, options.value_if_empty));
+        if (div.style) {
+        	div.style.color='';
+        }
+        return this._super(value, this, '');
     },
-  });
+    });
+    
+  openerp.web.page.readonly.add('colored', 'openerp.web.page.FieldColoredReadonly');
 
-  instance.web.list.columns.add('field.colored', 'instance.web.list.ColoredColumn');
+  //########################################################
+  var old_format_cell = openerp.web.format_cell;
+  
+  openerp.web.format_cell = function (row_data, column, options) {
+  	options = options || {};
+    var attrs = {};
+    if (options.process_modifiers !== false) {
+        attrs = column.modifiers_for(row_data);
+    }
+    if (attrs.invisible) { return ''; }
+
+    if ((column.tag === 'button') || (!row_data[column.id])) {
+    	return old_format_cell(row_data, column, options);
+    }
+    
+    if (column.widget == 'colored') {
+    	var colored = JSON.parse(column.modifiers || '{}').colored;
+   	    if (row_data[column.id].value && colored) {
+            for (var color in colored)
+            {
+          	    var expr = colored[color];
+          	    expr_bool = eval(row_data[column.id].value + expr);
+          	    if (expr_bool == true) {
+          		    return _.str.sprintf('<span style="color:%s">%s</span>', color, row_data[column.id].value);
+          	    }
+            }
+        }
+    }
+    return old_format_cell(row_data, column, options);
+  };
+
 
 };
