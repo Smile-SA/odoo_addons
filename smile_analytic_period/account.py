00c395f7105568cedcddc97d01c296dadcd24f07
Kevin Deldycke <kevin@deldycke.com>
2013-04-10 13:38:07 +0000

    Update copyright dates.

--- smile_analytic_period/account.py
+++ smile_analytic_period/account.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2011-2012 Smile (<http://www.smile.fr>).
+#    Copyright (C) 2011-2013 Smile (<http://www.smile.fr>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
