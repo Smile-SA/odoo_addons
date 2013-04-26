5446342f58b3479ad1c0e95224b7c41d22c2204a
Corentin PB <corentin.pb@gmail.com>
2013-04-26 10:58:38 +0200

    [FIX] Correct get_display_precision method

--- smile_decimal_precision/decimal_precision.py
+++ smile_decimal_precision/decimal_precision.py
@@ -58,5 +58,8 @@ class DecimalPrecision(osv.osv):
 
     @staticmethod
     def get_display_precision(cr, uid, application):
-        res = pooler.get_pool(cr.dbname).get('decimal.precision').display_precision_get(cr, uid, application)
+        res = 2
+        dp_obj = pooler.get_pool(cr.dbname).get('decimal.precision')
+        if hasattr(dp_obj, 'display_precision_get'):
+            res = dp_obj.display_precision_get(cr, uid, application)
         return (16, res)
