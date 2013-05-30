4043d4a8175a20e7d2b757ea5881dd6fd589695e
Corentin PB <corentin.pb@gmail.com>
2013-05-30 17:36:26 +0200

    [ADD] Add smile_hr_contract

---
+++ smile_hr_contract/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Smile (<http://www.smile.fr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Contract Management",
+    "version": "0.1",
+    "depends": ["hr_contract"],
+    "author": "Smile",
+    "description": """
+    This module improve employee contract management.
+
+    This module built and test on OpenERP v7.0.
+
+    Suggestions & Feedback to: kevin.deldycke@smile.fr & corentin.pouhet-brunerie@smile.fr
+    """,
+    "website": "http://www.smile.fr",
+    "category": 'Human Resources',
+    "sequence": 20,
+    "init_xml": [],
+    "update_xml": [],
+    'demo_xml': [],
+    'test': [],
+    "auto_install": True,
+    "installable": True,
+    "application": False,
+}
