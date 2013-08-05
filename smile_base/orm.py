0e4bfa096bda4c0d82c0bb30fee34abb193cd82e
Corentin PB <corentin.pb@gmail.com>
2013-08-05 14:30:03 +0200

    [IMP] Override unlink to avoid to bypass orm with ondelete='cascade'

--- smile_base/orm.py
+++ smile_base/orm.py
@@ -21,6 +21,8 @@
 
 from openerp.osv.orm import BaseModel
 
+native_import_data = BaseModel.import_data
+native_unlink = BaseModel.unlink
 native_validate = BaseModel._validate
 
 
@@ -30,17 +32,12 @@ def new_validate(self, cr, uid, ids, context=None):
         return
     native_validate(self, cr, uid, ids, context)
 
-BaseModel._validate = new_validate
-
 
 # Helper function combining _store_get_values and _store_set_values
 def _compute_store_set(self, cr, uid, ids, context):
     """
-    get the list of stored function field to recompute (via _store_get_values)
+    Get the list of stored function field to recompute (via _store_get_values)
     and recompute them (via _store_set_values)
-
-    mainly useful to avoid useless (and costly) write calls in the create
-    (see timesheet line create for example)
     """
     store_get_result = self._store_get_values(cr, uid, ids, self._columns.keys(), context)
     store_get_result.sort()
@@ -58,13 +55,30 @@ def _compute_store_set(self, cr, uid, ids, context):
         self.pool.get(model)._store_set_values(cr, uid, todo, fields_to_recompute, context)
 
 
-BaseModel._compute_store_set = _compute_store_set
+def new_import_data(self, cr, uid, fields, datas, mode='init', current_module='', noupdate=False, context=None, filename=None):
+    context_copy = context and context.copy() or {}
+    context_copy['defer_parent_store_computation'] = True
+    return native_import_data(self, cr, uid, fields, datas, mode, current_module, noupdate, context_copy, filename)
+
+
+def new_unlink(self, cr, uid, ids, context=None):
+    """Force unlink for remote fields.many2one with ondelete='cascade'"""
+    if hasattr(self, '_cascade_relations'):
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        for model, fnames in self._cascade_relations.iteritems():
+            domain = ['|'] * (len(fnames) - 1) + [(fname, 'in', ids) for fname in fnames]
+            sub_model_obj = self.pool.get(model)
+            sub_model_ids = sub_model_obj.search(cr, uid, domain, context=context)
+            sub_model_obj.unlink(cr, uid, sub_model_ids, context)
+    return native_unlink(self, cr, uid, ids, context)
 
 
 def bulk_create(self, cr, uid, vals_list, context=None):
     context_copy = context and context.copy() or {}
     context_copy['no_store_function'] = True
     context_copy['no_validate'] = True
+    context_copy['defer_parent_store_computation'] = True
     ids = []
     if not isinstance(vals_list, list):
         vals_list = [vals_list]
@@ -72,8 +86,12 @@ def bulk_create(self, cr, uid, vals_list, context=None):
         ids.append(self.create(cr, uid, vals, context_copy))
     self._compute_store_set(cr, uid, ids, context)
     self._validate(cr, uid, ids, context)
+    self._parent_store_compute(cr)
     return True
 
+BaseModel._compute_store_set = _compute_store_set
+BaseModel._validate = new_validate
 BaseModel.bulk_create = bulk_create
-
+BaseModel.import_data = new_import_data
 BaseModel.store_set_values = BaseModel._store_set_values
+BaseModel.unlink = new_unlink
