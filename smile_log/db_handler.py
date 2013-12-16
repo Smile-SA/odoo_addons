72447a41462b2948469e199a4b4c75e97e55ca26
corentin.pb <corentin.pb@gmail.com>
2013-12-16 16:03:58 +0100

    [IMP] Decode stack

--- smile_log/db_handler.py
+++ smile_log/db_handler.py
@@ -91,7 +91,7 @@ def add_trace(original_method):
     def new_method(self, msg):
         stack = traceback.format_exc()
         stack = stack.replace('%', '%%')
-        msg += '\n%s' % stack
+        msg += '\n%s' % stack.decode('utf-8')
         return original_method(self, msg)
     return new_method
 
