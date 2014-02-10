c594d708632092524e7e3d3a1fc7872cee7589ee
corentin.pb <corentin.pb@gmail.com>
2014-02-10 15:14:32 +0100

    [IMP] Add FixedLengthExport class

--- smile_utils/exports.py
+++ smile_utils/exports.py
@@ -19,16 +19,63 @@
 #
 ##############################################################################
 
+from decimal import Decimal
+import logging
 import unicodedata
 
-
-def strip_accents(msg):
-    return ''.join((c for c in unicodedata.normalize('NFD', msg) if unicodedata.category(c) != 'Mn'))
+_logger = logging.getLogger(__package__)
 
 
-def replace_non_ascii_by_space(msg):
-    return "".join([ord(i) < 128 and i or ' ' for i in msg]).encode('ascii')
+def strip_accents(string):
+    return ''.join((c for c in unicodedata.normalize('NFD', string) if unicodedata.category(c) != 'Mn'))
 
 
-def clean_string(msg):
-    return replace_non_ascii_by_space(strip_accents(unicode(msg)).upper())
+def replace_non_ascii_by_space(string):
+    return "".join([ord(i) < 128 and i or ' ' for i in string]).encode('ascii')
+
+
+def clean_string(string):
+    return replace_non_ascii_by_space(strip_accents(unicode(string)).upper())
+
+
+class FixedLengthExport(object):
+
+    def __init__(self, export_format, delimiter=''):
+        assert isinstance(export_format, dict), 'export_format must be a dictionary with values of type tuple (position, type, length)'
+        self._format = export_format
+        self._columns = sorted(export_format, key=lambda x: export_format[x][0])
+        self._delimiter = delimiter
+
+    def _truncate(self, value, length):
+        if len(value) > length:
+            _logger.warning("FixedLengthExport: %s value is too long (max. length = %s)" % (value, length))
+        return value[:length]
+
+    def _format_string(self, value, length):
+        value = clean_string(value or '').ljust(length)
+        return self._truncate(value, length)
+
+    def _format_integer(self, value, length):
+        if isinstance(value, float):
+            value = str(int(round(value)))
+        value = str(value or 0).rjust(length, '0')[:length]
+        return self._truncate(value, length)
+
+    def _format_float(self, value, length):
+        value = value or 0.0
+        assert isinstance(value, (float, Decimal)), 'value must be a float: %s' % (value)
+        format_string = '%%0%dd' % (length,)
+        value = format_string % int(round((value or 0.0) * 100))
+        return self._truncate(value, length)
+
+    def format_row(self, data):
+        res = []
+        for column in self._columns:
+            position, ttype, length = self._format[column]
+            format_method_name = '_format_%s' % ttype
+            if hasattr(self, format_method_name):
+                format_method = getattr(self, format_method_name)
+            else:
+                format_method = self._truncate
+            res.append(format_method(data.get(column), length))
+        return self._delimiter.join(res)
