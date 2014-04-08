1bbce430ba824438cbb073718aa6066f844769a8
corentin.pb <corentin.pb@gmail.com>
2014-04-08 09:11:31 +0200

    [FIX] Correct detective_dispatch signature

--- smile_base/service.py
+++ smile_base/service.py
@@ -36,14 +36,14 @@ _requests = service._requests
 
 def smile_detective(min_delay):  # min_delay in seconds
     def detective_log(dispatch_func):
-        def detective_dispatch(self, method, params):
+        def detective_dispatch(method, params):
             pid = uuid.uuid4()
             db, uid = params[0:2]
             param_str = repr(params[3:])
             start = time.time()
             _requests[pid] = (db, uid, method, param_str, start)
             try:
-                result = dispatch_func(self, method, params)
+                result = dispatch_func(method, params)
             finally:
                 del _requests[pid]
             delay = time.time() - start
