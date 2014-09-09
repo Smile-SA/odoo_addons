5330577413f22ddd1c01a331aba31587cd1f4c3e
Corentin POUHET-BRUNERIE <corentin.pouhet-brunerie@smile.fr>
2014-09-10 00:51:55 +0200

    [IMP] Ban vcs and url modification for a repository which has a branch done

--- smile_scm/models/scm.py
+++ smile_scm/models/scm.py
@@ -79,12 +79,23 @@ class Repository(models.Model):
     _description = 'Repository'
     _inherit = ['mail.thread']
 
+    @api.one
+    def _has_branch_done(self):
+        if self.branch_ids and \
+                self.branch_ids.filtered(lambda b: b.state == 'done'):
+            self.has_branch_done = True
+        else:
+            self.has_branch_done = False
+
     name = fields.Char(required=True)
-    vcs_id = fields.Many2one('scm.vcs', 'Version Control System', required=True, ondelete='restrict')
+    vcs_id = fields.Many2one('scm.vcs', 'Version Control System',
+                             required=True, ondelete='restrict')
     url = fields.Char(size=256, required=True)
     partner_id = fields.Many2one('res.partner', 'Owner')
     tag_ids = fields.Many2many('scm.repository.tag', string="Tags")
     branch_ids = fields.One2many('scm.repository.branch', 'repository_id', 'Branches')
+    has_branch_done = fields.Boolean('Has at least a branch done',
+                                     compute='_has_branch_done', store=False)
 
     _sql_constraints = [
         ('unique_url', 'UNIQUE(url)', 'Repository URL must be unique'),
