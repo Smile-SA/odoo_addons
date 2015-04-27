c3bd6c7fdd4fdd956b9e093a4b6ce59270cabde3
simon <simon@odonna14.04>
2015-04-27 17:37:19 +0200

    smile_document

---
+++ smile_document/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Smile (<http://www.smile.fr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Smile Document',
+    'version': '1.0',
+    'depends': [
+        'document',
+    ],
+    'author': 'Smile',
+    'description': """
+        Document Management
+    """,
+    'summary': 'Smile Document',
+    'website': 'http://www.smile.fr',
+    'category': 'Document',
+    'sequence': 10,
+    'data': [
+        'views/document_view.xml',
+        'security/document_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo_xml': [],
+    'test': [],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
---
+++ smile_document/models/document.py
@@ -0,0 +1,86 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Smile (<http://www.smile.fr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from datetime import datetime
+
+from openerp import models, fields, api, exceptions
+from openerp.tools.translate import _
+
+
+STATUS = [('valid', 'Valid'),
+          ('expired', 'Expired'),
+          ('archived', 'Archived')]
+
+
+class IrAttachementType(models.Model):
+    _name = 'ir.attachment.type'
+
+    name = fields.Char(required=True, translate=True)
+
+    _sql_constraints = [
+        ('unique_name', 'UNIQUE (name)', _('Document type name must be unique')),
+    ]
+
+    @api.multi
+    def unlink(self):
+        if self._context.get('force_unlink_doc_type'):
+            return super(IrAttachementType, self).unlink()
+        raise exceptions.Warning(_('Attention : You cannot unlink document type!'))
+
+
+class IrAttachement(models.Model):
+    _inherit = 'ir.attachment'
+
+    document_type_id = fields.Many2one('ir.attachment.type', string="Document Type")
+    document_date = fields.Date(default=fields.datetime.now())
+    expiry_date = fields.Date()
+    archived = fields.Boolean()
+    status = fields.Selection(STATUS, readonly=True, default='valid')
+
+    @api.multi
+    def _compute_document_status(self):
+        for doc in self:
+            status = 'valid'
+            today = datetime.now().date()
+            if doc.expiry_date:
+                expiry_date = datetime.strptime(doc.expiry_date, "%Y-%m-%d").date()
+                if expiry_date >= today and not doc.archived:
+                    status = 'valid'
+                elif expiry_date < today and not doc.archived:
+                    status = 'expired'
+            if doc.archived:
+                status = 'archived'
+                if doc.expiry_date and datetime.strptime(doc.expiry_date, "%Y-%m-%d").date() > today:
+                    doc.expiry_date = today
+            if doc.status != status:
+                doc.status = status
+
+    @api.model
+    def create(self, values):
+        res = super(IrAttachement, self).create(values)
+        res._compute_document_status()
+        return res
+
+    @api.multi
+    def write(self, values):
+        res = super(IrAttachement, self).write(values)
+        self._compute_document_status()
+        return res
