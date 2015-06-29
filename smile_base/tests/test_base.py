516bc8419a376a7f430aeff1b034ee56bf2f8e90
Isabelle RICHARD <isabellerichard89@gmail.com>
2015-06-29 18:19:47 +0200

    [TEST] Add test for enable/disable email sending

--- smile_base/tests/test_base.py
+++ smile_base/tests/test_base.py
@@ -19,7 +19,24 @@
 #
 ##############################################################################
 
+from contextlib import contextmanager
+
 from openerp.tests.common import TransactionCase
+from openerp.tools.config import config
+from openerp.addons.base.ir.ir_mail_server import MailDeliveryException
+
+
+@contextmanager
+def config_to_enable_email_sending(enable_email_sending):
+    init_config = {
+        'enable_email_sending': config.get('enable_email_sending'),
+    }
+    try:
+        config['enable_email_sending'] = enable_email_sending
+        yield config
+    finally:
+        for key, value in init_config.iteritems():
+            config[key] = value
 
 
 class BaseTest(TransactionCase):
@@ -29,6 +46,10 @@ class BaseTest(TransactionCase):
         self.model = self.env['res.partner.category']
 
     def test_bulk_create(self):
+        """
+            I create three categories with the same method call
+            I check that three categories were created
+        """
         names = ['t1', 't2', 't3']
         vals_list = [{'name': name} for name in names]
         categories = self.model.bulk_create(vals_list)
@@ -41,3 +62,37 @@ class BaseTest(TransactionCase):
         child = self.model.create({'name': 'Child', 'parent_id': parent.id})
         self.assertTrue(parent.unlink())
         self.assertFalse(child.exists())
+
+    def _get_email(self):
+        vals = {
+            'subject': 'The subject',
+            'body': 'and the body',
+            'email_from': 'admin@example.org',
+            'email_to': 'demo@example.org',
+        }
+        return self.env['mail.mail'].create(vals)
+
+    def test_disable_email_sending(self):
+        """
+            I disable email sending in config file
+            I send an email
+            I check that no email was sent
+        """
+        email = self._get_email()
+        with config_to_enable_email_sending(False):
+            email.send()
+            self.assertEquals('exception', email.state, 'Email should be in exception!')
+
+    def test_enable_email_sending(self):
+        """
+            I enable email sending in config file
+            I send an email
+            I check that email was sent
+        """
+        # I remove all outgoing mail server to be sure the exception will be raised
+        self.env['ir.mail_server'].search([]).unlink()
+        email = self._get_email()
+        with config_to_enable_email_sending(True):
+            # TODO: see why exception is not catched
+            with self.assertRaises(MailDeliveryException):
+                email.send()
