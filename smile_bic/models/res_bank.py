4a46fb69e0b7b212a659edb2c79d772d4b3f6147
Corentin POUHET-BRUNERIE <corentin.pouhet-brunerie@smile.fr>
2016-01-15 08:55:49 +0100

    [IMP] Add smile_bic

---
+++ smile_bic/models/res_bank.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from openerp import api, models, _
+from openerp.exceptions import Warning
+
+
+class ResBank(models.Model):
+    _inherit = 'res.bank'
+
+    @api.one
+    @api.constrains('bic')
+    def _check_bic(self):
+        # ISO 9362:2009
+        bic_check = re.compile(r'^[A-Z]{4}[A-Z]{2}[A-Z0-9]{2}([A-Z0-9]{3})?$')
+        if self.bic and not bic_check.match(self.bic):
+            raise Warning(_('Incorrect BIC/SWIFT'))
