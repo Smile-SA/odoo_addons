bf9187b7dd2f47e47898ccc45c3c84dd9269d5f2
Matthieu JOOSSEN <matthieu@joossen.name>
2016-05-25 22:33:43 +0200

    [ADD]smile event event calendar module
    This module add link between event event and calendar event

---
+++ smile_event_event_calendar/models/calendar_event.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 Smile (<http://www.smile.fr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning
+
+
+class CalendarEvent(models.Model):
+    _inherit = 'calendar.event'
+
+    event_event_id = fields.Many2one('event.event', string='Event', ondelete='restrict', readonly=True)
+
+    @api.multi
+    def write(self, vals):
+        event_ids = self.mapped('event_event_id')
+        event_vals = {}
+        if 'start_datetime' in vals:
+            event_vals['date_begin'] = vals.get('start_datetime')
+        if 'stop_datetime' in vals:
+            event_vals['date_end'] = vals.get('stop_datetime')
+        if event_ids and not all(event_vals):
+            raise Warning(_('You cannot remove start or stop date if one of the current record have and event'))
+        res = super(CalendarEvent, self).write(vals)
+        if event_ids and event_vals and not self._context.get('from_event'):
+            event_ids.with_context(from_calendar=True).write(event_vals)
+        return res
---
+++ smile_event_event_calendar/models/event.py
@@ -0,0 +1,73 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 Smile (<http://www.smile.fr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+
+
+class EventEvent(models.Model):
+    _inherit = 'event.event'
+
+    calendar_event_id = fields.Many2one('calendar.event', 'Calendar Event', ondelete='restrict', readonly=True)
+
+    @api.model
+    def create(self, vals):
+        event = super(EventEvent, self).create(vals)
+        calendar_event_vals = {'name': event.name,
+                               'start_datetime': event.date_begin,
+                               'stop_datetime': event.date_end,
+                               'start': event.date_begin,
+                               'stop': event.date_end,
+                               'allday': False,
+                               'location': event.address_id and event.address_id.name,
+                               'class': 'public',
+                               'show_as': 'free',
+                               'partner_ids': [(6, 0, [])],
+                               'event_event_id': event.id, }
+        ce_id = self.env['calendar.event'].create(calendar_event_vals)
+        event.calendar_event_id = ce_id
+        return event
+
+    @api.multi
+    def write(self, vals):
+        calendar_event_vals = {}
+        onchange_date = []
+        if 'caldendar_event_id' in vals:
+            del vals['calendar_event_id']
+        if vals.get('name'):
+            calendar_event_vals['name'] = vals.get('name')
+        if vals.get('date_begin'):
+            calendar_event_vals['start_datetime'] = vals.get('date_begin')
+            onchange_date.append('start')
+        if vals.get('date_end'):
+            calendar_event_vals['stop_datetime'] = vals.get('date_end')
+            onchange_date.append('stop')
+        if vals.get('address_id'):
+            calendar_event_vals['location'] = self.env['res.partner'].sudo().browse(vals.get('address_id')).name
+        res = super(EventEvent, self).write(vals)
+        if calendar_event_vals and not self._context.get('from_calendar'):
+            self.calendar_event_id.write(calendar_event_vals)
+            allday = self.calendar_event_id.allday
+            for fromtype in onchange_date:
+                start = calendar_event_vals.get('start_datetime', False)
+                end = calendar_event_vals.get('stop_datetime', False)
+                self.calendar_event_id.with_context(from_event=True).onchange_dates(fromtype, start, end, allday,
+                                                                                    allday)
+        return res
