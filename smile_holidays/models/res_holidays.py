e8ab818c7575138bc059cbfa5eacbcf94dd484d0
Isabelle RICHARD <isabelle.richard@smile.fr>
2016-08-14 17:01:31 +0200

    [MIG] Migrate smile_holidays

---
+++ smile_holidays/models/res_holidays.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Smile (<http://www.smile.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api, fields, models, _
+from openerp.exceptions import Warning
+
+
+class ResHolidaysArea(models.Model):
+    _name = 'res.holidays.area'
+    _description = 'Holidays Area'
+
+    name = fields.Char(required=True)
+    holiday_date_ids = fields.Many2many('res.holidays.date', 'res_holidays_area_res_holiday_date_rel',
+                                        'area_id', 'date_id', 'Holidays Dates')
+    attribution_ids = fields.One2many('res.holidays.attribution', 'area_id', "Attributions")
+
+
+class ResHolidaysDate(models.Model):
+    _name = 'res.holidays.date'
+    _description = 'Holidays Date'
+
+    name = fields.Char(required=True)
+    area_ids = fields.Many2many('res.holidays.area', 'res_holidays_area_res_holiday_date_rel',
+                                'date_id', 'area_id', 'Holidays Areas')
+    start_date = fields.Date(required=True)
+    end_date = fields.Date(required=True)
+
+    @api.one
+    @api.constrains('start_date', 'end_date', 'area_ids')
+    def _check_dates(self):
+        if self.start_date > self.end_date:
+            raise Warning(_("Start Date can't be after End Date!"))
+        for area in self.area_ids:
+            holidays_dates = self.search(['&', '&', ('area_ids', 'in', area.id), ('id', '!=', self.id), '|',
+                                          '&', ('start_date', '>=', self.start_date), ('start_date', '<=', self.end_date), '|',
+                                          '&', ('end_date', '<=', self.end_date), ('end_date', '>=', self.start_date),
+                                          '&', ('start_date', '<=', self.start_date), ('end_date', '>=', self.end_date)])
+            if holidays_dates:
+                holiday = holidays_dates[0]
+                raise Warning(_("The holiday period {} is already declared for zone {} between {} and {}")
+                              .format(holiday.name, area.name, holiday.start_date, holiday.end_date,))
+
+    @api.multi
+    def name_get(self):
+        return [(date.id, _("(%s) From %s to %s") % (date.name, date.start_date, date.end_date))
+                for date in self]
+
+
+class ResHolidaysAttribution(models.Model):
+    _name = 'res.holidays.attribution'
+    _description = 'Holidays Attribution'
+    _order = 'effective_date, department_id'
+
+    area_id = fields.Many2one('res.holidays.area', 'Holidays Area', required=True, ondelete='restrict')
+    department_id = fields.Many2one('res.country.department', 'Department', required=True,
+                                    ondelete='restrict')
+    effective_date = fields.Date(required=True,
+                                 help="The area starts to be active at this date")
+
+
+class ResCountryDepartment(models.Model):
+    _inherit = 'res.country.department'
+
+    attribution_ids = fields.One2many('res.holidays.attribution', 'department_id', 'Effective areas')
+
+    @api.one
+    @api.returns('res.holidays.area', lambda area: area.id)
+    def get_effective_area_at_a_date(self, date):
+        """
+        Returns the effective area for this department at a date.
+
+        @param date: fields.Str
+        @return: res.holidays.area
+        """
+        res = self.env['res.holidays.area'].browse()
+        attribution_ids = self.attribution_ids.filtered(lambda attribution:
+                                                        attribution.effective_date <= date)
+        if attribution_ids:
+            res = attribution_ids.sorted(lambda self: self.effective_date, reverse=True)[0].area_id
+        return res
---
+++ smile_holidays/tests/test_holidays.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Smile (<http://www.smile.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.tests.common import TransactionCase
+from openerp.exceptions import ValidationError
+
+
+class TestHolidays(TransactionCase):
+    """
+    """
+
+    def setUp(self):
+        super(TestHolidays, self).setUp()
+
+        # User profile
+        vals = {
+            'name': 'Holidays Admin',
+            'login': 'holidays_admin',
+            'lang': 'en_US',
+            'groups_id': [(4, self.ref('base.group_no_one'))],
+        }
+        self.holidays_admin = self.env['res.users'].create(vals)
+
+        # Data
+        self.area_a = self.env.ref('smile_holidays.res_holidays_area_a')
+        self.area_b = self.env.ref('smile_holidays.res_holidays_area_b')
+        self.department = self.env.ref('l10n_fr_department.res_country_department_aisne')
+        for attribution in self.department.attribution_ids:
+            self.department.write({'attribution_ids': [(2, attribution.id, 0)]})
+
+    def test_invalid_holiday_dates(self):
+        """
+            I check that I can't create invalid holiday dates.
+        """
+        date_obj = self.env['res.holidays.date']
+        vals = {
+            'area_ids': [(4, self.area_a.id)],
+            'start_date': '2015-10-25',
+            'end_date': '2015-09-04', 'name': 'x',
+        }
+        with self.assertRaisesRegexp(ValidationError, "Start Date can't be after End Date!"):
+            date_obj.sudo(self.holidays_admin).create(vals)
+
+    def test_get_area_of_a_department_at_a_date(self):
+        """
+            I assign two areas to a department:
+            - the effective date of the area A is 2015-01-01
+            - the effective date of the area B is 2015-07-01
+            I call the area effective for the departement at 2015-08-01
+            I check that the returned area is B
+            I call the area effective for the department at 2015-02-01
+            I check that the returned area is A
+        """
+        attribution_obj = self.env['res.holidays.attribution']
+        vals_list = [
+            {'area_id': self.area_a.id, 'department_id': self.department.id,
+             'effective_date': '2015/01/01'},
+            {'area_id': self.area_b.id, 'department_id': self.department.id,
+             'effective_date': '2015/07/01'},
+        ]
+        attribution_obj.bulk_create(vals_list)
+        res = self.department.get_effective_area_at_a_date('2015-08-01')
+        self.assertEquals(self.area_b, res, 'The expected area is B!')
+        res = self.department.get_effective_area_at_a_date('2015-02-01')
+        self.assertEquals(self.area_a, res, 'The expected area is A!')
