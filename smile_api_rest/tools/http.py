abebcacc44192cc0174c71317d208c3b377a6f55
Corentin POUHET-BRUNERIE <corentin.pouhet-brunerie@smile.fr>
2017-09-11 12:02:58 +0200

    [IMP] Manage exceptions

--- smile_api_rest/tools/http.py
+++ smile_api_rest/tools/http.py
@@ -12,8 +12,11 @@ class make_response():
     def __call__(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            return request.make_response(json.dumps(result))
+            try:
+                result = func(*args, **kwargs)
+                return request.make_response(json.dumps(result))
+            except Exception, e:
+                return request.make_response(json.dumps({'error': str(e)}))
         return wrapper
 
 
