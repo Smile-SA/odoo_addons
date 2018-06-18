ad9d123af4a0df5046dcadbde202c249b3e24157
Corentin POUHET-BRUNERIE <corentin.pb@gmail.com>
2018-06-18 09:19:22 +0200

    [IMP] smile_data_integration: correct the default value of ResUsers.context_active_test

--- smile_data_integration/models/res_users.py
+++ smile_data_integration/models/res_users.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    context_active_test = fields.Boolean()
+    context_active_test = fields.Boolean(default=True)
     context_raise_load_exceptions = fields.Boolean()
