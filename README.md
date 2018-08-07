45e449ebfa3578a0334f72f8b7048c18292830a4
Isabelle RICHARD <isabelle.richard@smile.fr>
2018-08-07 14:15:00 +0200

    Initialize branch with Odoo 10.0EE modules developed by Smile

---
+++ README.md
@@ -0,0 +1,9 @@
+Smile addons for Odoo (formerly OpenERP)
+========================
+
+This repository contains a collection of Odoo modules.
+
+Requirements
+------------------------
+
+* Odoo 10.0 EE
---
+++ smile_web_gantt/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# (C) 2018 Smile (<http://www.smile.fr>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Web Gantt",
+    "version": "0.1",
+    "category": "Hidden",
+    "depends": [
+        "web_gantt",
+    ],
+    "author": "Smile",
+    "license": 'LGPL-3',
+    "description": """
+Web Gantt Extension
+===================
+
+* Allow to display weeks of year
+
+Suggestions & Feedback to: corentin.pouhet-brunerie@smile.fr
+    """,
+    "website": "http://www.smile.fr",
+    "sequence": 0,
+    "data": [
+        "views/webclient_templates.xml",
+    ],
+    "demo": [],
+    "auto_install": True,
+    "installable": True,
+    "application": False,
+}
