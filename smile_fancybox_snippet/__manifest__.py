0928dcba70de428630c205ad8005e2bd5a446314
Youssef ECHCHERIFELKETTANI <youssef.echcherifelkettani@smile-maroc.com>
2018-08-28 11:21:08 +0100

    UPD + smile_fancybox_snippet

---
+++ smile_fancybox_snippet/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Fancybox Snippet',
+
+    'author': 'Smile',
+
+    'summary': 'A snippet implementing jquery fancybox',
+
+    'depends': ['website'],
+
+    'data': ['views/assets.xml',
+             'views/fancybox_snippet.xml']
+}
