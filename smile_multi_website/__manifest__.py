de3d51f8cfd55ca28ddc7bb4686d65afedfea5e1
Dhosy Jonathan <jodho@smile.fr>
2018-08-30 10:15:14 +0200

    [Add] multi website module

---
+++ smile_multi_website/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today OpenSur.
+# (C) 2015 Smile (<http://www.smile.fr>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Smile Multi Website',
+    'category': 'Website',
+    'website': 'https://www.smile.fr',
+    'summary': 'Add website configuration menu',
+    'version': '1.00',
+    'description': """
+Smile Multi Website
+====================
+Add access to the website model with a new menu.
+Check the readme for the configuration and usage.
+        """,
+    'author': 'Smile SA',
+    'license': 'AGPL-3',
+    'depends': ['website'],
+    'data': ['views/website.xml'],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+}
