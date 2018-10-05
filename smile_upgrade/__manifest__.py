723d5d9352eca757a0a639c2ce482693d7b4e5af
Isabelle RICHARD <isabelle.richard@smile.fr>
2018-10-05 18:33:07 +0200

    Add documentation for smile_upgrade

--- smile_upgrade/__manifest__.py
+++ smile_upgrade/__manifest__.py
@@ -8,67 +8,7 @@
     "depends": ["web"],
     "author": "Smile",
     "license": 'AGPL-3',
-    "description": """Smile Upgrade
-
-Features
-
-    * Allow to upgrade automatically database
-    after code update and server restarting
-
-Execution
-
-    odoo.py -c <config_file> -d <db_name> --load=web,smile_upgrade
-
-Configuration
-
-    * Upgrades structure
-        <project_directory>
-        `-- upgrades
-            |-- 1.1
-            |   |-- __upgrade__.py
-            |   |-- *.sql
-            |   |-- *.yml                   # only for post-load
-            |   |-- *.csv                   # only for post-load
-            |   `-- *.xml                   # only for post-load
-            |-- 1.2
-            |   |-- __upgrade__.py
-            |   `-- *.sql
-            `-- upgrade.conf
-
-    * Upgrades configuration -- upgrade.conf
-        [options]
-        version=1.2
-
-    * Upgrade configuration -- __upgrade__.py
-        Content: dictonary with the following keys:
-            * version
-            * databases: let's empty if valid for all databases
-            * translations_to_reload: language codes list to reload
-            in post-load
-            * description
-            * modules_to_install_at_creation: modules list to install
-            at database creation
-            * modules_to_upgrade: modules list to update or install
-            * pre-load: list of .sql files
-            * post-load: list with .sql, .yml, .csv and .xml files
-                         with path .../filename (depending upgrades_path)
-                         or module_name/.../filename
-
-    * Odoo server configuration -- rcfile=~/.odoo_serverrc
-        [options]
-        upgrades_path = <project_directory>
-        stop_after_upgrades = True if you want to stop server after upgrades
-            else False
-
-Additional features
-
-    * In post-load, you can replace filename string by tuple
-      (filename,
-       'rollback_and_continue' or 'not_rollback_and_continue' or 'raise')
-      -- default value = 'raise'
-
-Suggestions & Feedback to: corentin.pouhet-brunerie@smile.fr
-    """,
+    "description": """""",
     "summary": "",
     "website": "http://www.smile.fr",
     "category": 'Hidden',
---
+++ smile_upgrade/demo/upgrades/0.1/__upgrade__.py
@@ -0,0 +1,16 @@
+{
+    'version': '0.1',
+    'description': """
+Put upgrade desciption here.
+    """,
+    'databases': [],
+    'modules_to_install_at_creation': [
+        'sale',
+        'purchase',
+    ],
+    'modules_to_upgrade': [
+        'sale',
+    ],
+    'pre-load': [],
+    'post-load': [],
+}
