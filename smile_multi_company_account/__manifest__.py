0b64afb0392761e330be10f5193113abe8bd9822
majda EL MARIOULI <majda.elmariouli@smile-maroc.com>
2018-11-14 14:57:27 +0100

    [ADD]: smile_multi_company_account: migration from V11.0 to V12.0 + Documentation

---
+++ smile_multi_company_account/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# (C) 2018 Smile (<http://www.smile.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Multi-company - Invoicing",
+    "version": "0.3",
+    "depends": [
+        "smile_multi_company_base",
+        "account",
+    ],
+    "author": "Smile",
+    "license": 'AGPL-3',
+    "description": """""",
+    "summary": "",
+    "website": "http://www.smile.fr",
+    "category": 'Hidden',
+    "sequence": 20,
+    "data": [
+        'views/res_company_view.xml',
+    ],
+    "qweb": [],
+    "auto_install": True,
+    "installable": True,
+    "application": False,
+}
