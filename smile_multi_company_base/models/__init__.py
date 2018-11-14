b719b48ddfa61bedba8200bc97d6d0d62ea65149
majda EL MARIOULI <majda.elmariouli@smile-maroc.com>
2018-11-14 15:55:47 +0100

    [ADD]: smile_multi_company_base: migration from V11.0 to V12.0 + Documentation

---
+++ smile_multi_company_base/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# (C) 2018 Smile (<http://www.smile.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import fields
+from . import ir_property
+from . import res_company
+from . import res_users
