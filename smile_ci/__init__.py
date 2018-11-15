878679bf1f3650afba2963b9abf0df241a7607c4
Corentin POUHET-BRUNERIE <corentin.pouhet-brunerie@smile.fr>
2018-11-15 13:55:44 +0100

    Update __init__.py

--- smile_ci/__init__.py
+++ smile_ci/__init__.py
@@ -2,3 +2,9 @@
 
 from . import contollers
 from . import models
+
+from odoo.addons.smile_docker.tools import exceptions as docker_exceptions
+from odoo.addons.smile_scm.tools import exceptions as scm_exceptions
+
+
+scm_exceptions.get_exception_message = docker_exceptions.get_exception_message
