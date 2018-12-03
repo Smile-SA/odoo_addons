3aafc463c70a0696c58a5152e113d5c10db9a3c0
majda EL MARIOULI <majda.elmariouli@smile-maroc.com>
2018-12-03 11:22:46 +0100

    [ADD]: smile_base_automation: migration from V11.0 to V12.0 + Documentation

---
+++ smile_base_automation/models/ir_model_methods.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# (C) 2018 Smile (<http://www.smile.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class IrModelMethods(models.Model):
+    _name = 'ir.model.methods'
+    _description = 'Model Method'
+    _order = 'name'
+
+    name = fields.Char(size=128, readonly=True, required=True, index=True)
+    model_id = fields.Many2one('ir.model', 'Model', readonly=True,
+                               required=True, index=True, ondelete='cascade')
+    is_public = fields.Boolean(compute='_is_public', store=True)
+
+    @api.one
+    def _is_public(self):
+        self.is_public = not self.name.startswith('_')
