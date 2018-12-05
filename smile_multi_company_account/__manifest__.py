2b3b414764590000096746624b4ff3dd19ed9f01
Majda EL MARIOULI <majda.elmariouli@smile-maroc.com>
2018-12-05 14:10:18 +0100

    [Add] smile_multi_company_account : README.rst and index.html

--- smile_multi_company_account/__manifest__.py
+++ smile_multi_company_account/__manifest__.py
@@ -28,12 +28,7 @@
     ],
     "author": "Smile",
     "license": 'AGPL-3',
-    "description": """
-Multi-company Invoicing
-=======================
-
-Suggestions & Feedback to: corentin.pouhet-brunerie@smile.fr
-    """,
+    "description": """""",
     "summary": "",
     "website": "http://www.smile.fr",
     "category": 'Hidden',
