f6de288231d9866d45f1789fcfbb8f57c92e572b
Corentin POUHET-BRUNERIE <corentin.pb@gmail.com>
2019-02-15 11:32:56 +0100

    [IMP] smile_upgrade: deactivate cron while upgrading

--- smile_upgrade/models/registry.py
+++ smile_upgrade/models/registry.py
@@ -50,6 +50,8 @@ def new(cls, db_name, force_demo=False, status=None, update_module=False):
                 if upgrades:
                     t0 = time.time()
                     _logger.info('loading %s upgrade...', upgrade_manager.code_version)
+                    max_cron_threads = config.get('max_cron_threads')
+                    config['max_cron_threads'] = 0
                     if not upgrade_manager.db_in_creation:
                         upgrade_manager.pre_load()
                         modules = upgrade_manager.modules_to_upgrade
@@ -61,6 +63,7 @@ def new(cls, db_name, force_demo=False, status=None, update_module=False):
                     native_new(db_name, force_demo, update_module=True)
                     upgrade_manager.post_load()
                     upgrade_manager.set_db_version()
+                    config['max_cron_threads'] = max_cron_threads
                     _logger.info('%s upgrade successfully loaded in %ss',
                                  upgrade_manager.code_version, time.time() - t0)
                 else:
