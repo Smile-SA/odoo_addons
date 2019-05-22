21a9aff7fdf4e3d52dace22c18d834e74e632774
marco <martin.courtois@smile.fr>
2019-05-22 10:20:49 +0200

    [ADD] #796275 - add module smile_bic

---
+++ smile_bic/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# (C) 2019 Smile (<http://www.smile.fr>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Smile BIC Checking",
+    "version": "0.1",
+    "license": 'AGPL-3',
+    "depends": [
+        "base_iban",
+    ],
+    "author": "Smile",
+    "description": """Smile BIC Checking
+
+    Suggestions & Feedback to: Corentin Pouhet-Brunerie
+    """,
+    "summary": "",
+    "website": "http://www.smile.fr",
+    "category": 'Accounting & Finance',
+    "sequence": 20,
+    "data": [],
+    'demo': [],
+    'test': [],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
