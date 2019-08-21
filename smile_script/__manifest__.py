faafbdeeaee4c1695c76b7eeb15b4c5b3de1a524
Martin COURTOIS <martin.courtois@smile.fr>
2019-08-21 17:45:32 +0200

    12.0 796260 smile script

---
+++ smile_script/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# (C) 2019 Smile (<http://www.smile.fr>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Maintenance scripts",
+    "version": "1.0",
+    "author": "Smile",
+    "website": 'http://www.smile.fr',
+    "category": "Tools",
+    "license": 'AGPL-3',
+    "description": """""",
+    "depends": ['smile_log'],
+    "data": [
+        "security/smile_script_security.xml",
+        "security/ir.model.access.csv",
+        "views/script_view.xml",
+    ],
+    "installable": True,
+    "active": False,
+}
