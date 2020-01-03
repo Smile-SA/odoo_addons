eec6dcd6c941e760a39028e1869501c438b44649
Isabelle RICHARD <isabelle.richard@smile.fr>
2020-01-03 16:32:07 +0100

    [MIG] Migrate smile_attachment to 13.0

---
+++ smile_attachment/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# (C) 2019 Smile (<http://www.smile.fr>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Search in attachments",
+    "version": "0.1",
+    "sequence": 100,
+    "category": "Tools",
+    "author": "Smile",
+    "license": 'LGPL-3',
+    "website": 'http://www.smile.fr',
+    "description": "",
+    "depends": ["attachment_indexation"],
+    "data": [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
