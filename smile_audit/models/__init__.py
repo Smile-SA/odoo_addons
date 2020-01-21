3b7859ff5232c0cee167e54f4148bbaacb345349
wafaa JAOUAHAR <wafaa.jaouahar@smile-maroc.com>
2020-01-21 08:59:19 +0100

    [MIG]: smile_audit : migration from V12.0 to V13.0 + unit tests

---
+++ smile_audit/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# (C) 2020 Smile (<http://www.smile.fr>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from . import audit_log
+from . import audit_rule
+from . import base
