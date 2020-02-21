6d9c953c42bad6da9300d0392479776d255cf1fc
Isabelle RICHARD <isabelle.richard@smile.fr>
2020-02-21 09:38:41 +0100

    [FIX] Fix error on users views after smile_access_control uninstallation

--- smile_access_control/__manifest__.py
+++ smile_access_control/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# (C) 2011 Smile (<http://www.smile.fr>)
+# (C) 2020 Smile (<http://www.smile.fr>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
@@ -8,32 +8,7 @@
     "author": "Smile",
     "license": 'LGPL-3',
     "category": "Tools",
-    "description": """
-Manage users thanks to user profiles
-------------------------------------
-
-This is a new way to manage your users' rights :
-you can manage users by functional profiles.
-
-Basically, a « profile » is a fictive user (res.users) tagged as a profile.
-
-It means that like before (with the basic rules of Odoo),
-you can add groups to your profile.
-
-You can associate a profile to your user. Or in an other way,
-you can add users by profile.
-
-You can also set the fields which are private per user or global for all users.
-
-You have choice to update or not in write mode for associated users,
-with field 'Update users' in profiles.
-
-NB : to test your profile, you need to set him as « active »,
-which will be disabled afterwards at the next update.
-
-Suggestions & Feedback to: corentin.pouhet-brunerie@smile.fr &
-matthieu.choplin@smile.fr
-""",
+    "description": "",
     "depends": ['base'],
     "data": [
         "views/res_users_view.xml",
@@ -44,4 +19,5 @@ matthieu.choplin@smile.fr
     "demo": [],
     "installable": True,
     "active": False,
+    "uninstall_hook": "uninstall_hook",
 }
