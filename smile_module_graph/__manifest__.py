8d487adb561827ae54e8515480fbc458f2655199
imane <imane.elhajmir@neopixl.com>
2021-12-06 17:10:05 +0100

    [IMP] #1097941 smile_module_graph : migration to V13.0

---
+++ smile_module_graph/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# (C) 2018 Smile (<http://www.smile.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Modules Graph",
+    "version": "13.0.1.0.0",
+    "depends": ["base"],
+    "author": "Smile",
+    "license": 'AGPL-3',
+    "description": """""",
+    "website": "http://www.smile.eu",
+    "category": "Hidden",
+    "sequence": 32,
+    "data": [
+        'security/ir.model.access.csv',
+        "wizard/ir_module_graph_wizard_view.xml",
+        "views/module_view.xml",
+    ],
+    "demo": [],
+    'test': [],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    'external_dependencies': {
+        'python': ['pydot'],
+    }
+}
