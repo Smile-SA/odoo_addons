e76343969db0a0ae2dcea0b963dd2b3ec0ef3e4b
imane <imane.elhajmir@neopixl.com>
2021-12-17 11:59:05 +0100

    [IMP] #1101095 smile_confirmation : migration to V13.0

---
+++ smile_confirmation/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# (C) 2020 Smile (<https://www.smile.eu>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Confirm/Alert pop-up before saving",
+    "version": "13.0.1.0.0",
+    "depends": ['web'],
+    "author": "Smile",
+    "license": 'AGPL-3',
+    "description": """
+    """,
+    "summary": "",
+    "website": "https://www.smile.eu",
+    "category": 'Tools',
+    "sequence": 20,
+    "data": [
+        'security/ir.model.access.csv',
+        'views/assets.xml',
+        'views/popup_message.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
---
+++ smile_confirmation/models/base.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# (C) 2020 Smile (<https://www.smile.eu>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+from odoo import models
+
+
+class BaseModel(models.AbstractModel):
+    _inherit = 'base'
+
+    def get_message_informations(self, values=False):
+        """
+            This function gives us the possibility to know
+            if we display the message or not
+            - In create self is empty
+            - In write self is not empty contains current ID
+            :param values:
+                - In create dictionary contains all recording
+                    information self is False
+                - In write we find only values changed
+            :type values: dict
+            :return: return dict object popup.message
+                (self.env['popup.message'].read())
+        """
+        PopupMessage = self.env['popup.message']
+        messages = PopupMessage.search([('model_id.model', '=', self._name)])
+        if len(messages) > 0:
+            object_popup = []
+            for msg in messages:
+                dict_popup = {
+                    'model_id': msg.model_id,
+                    'model': msg.model,
+                    'field_ids': msg.field_ids,
+                    'field_name': msg.field_name,
+                    'popup_type': msg.popup_type,
+                    'title': msg.title,
+                    'message': msg.message,
+                    'active': msg.active,
+                }
+                object_popup.append(dict_popup)
+            return object_popup
+        else:
+            return False
+
+    def execute_processing(self, values=False):
+        """
+            This function gives us the possibility to execute a
+                specific treatment after the confirmation of the message
+            - In create self is empty
+            - In write self is not empty contains current ID
+            :param values : a list of dictionaries:
+                {'name': field, 'value': value of field}
+            :type dictionary list
+            :return boolean
+        """
+        return False
