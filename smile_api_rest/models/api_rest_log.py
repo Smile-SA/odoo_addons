8f5a5dbe9daf3acfcc6409ad20aa18a8145d3490
Julien DRECQ <julien.drecq@smile.fr>
2022-01-26 15:35:51 +0100

    [IMP] Add field summary response data & button to download response data when files are too big

--- smile_api_rest/models/api_rest_log.py
+++ smile_api_rest/models/api_rest_log.py
@@ -2,8 +2,13 @@
 # (C) 2021 Smile (<http://www.smile.fr>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+import base64
+
+from odoo import fields, models, api
 from odoo.http import request
+
+
+RESPONSE_DATA_MAX_CHARACTERS = 5000
 
 
 class ApiRestLog(models.Model):
@@ -16,6 +21,31 @@ class ApiRestLog(models.Model):
     request_headers = fields.Char()
     request_data = fields.Char()
     response_data = fields.Char()
+    length_response_data = fields.Integer(
+        compute='_get_infos_response_data', store=True)
+    summary_response_data = fields.Text(
+        'Details', compute='_get_infos_response_data', store=True)
+    file_response_data = fields.Binary(
+        compute='_get_infos_response_data', store=True)
+    filename_response_data = fields.Char(
+        compute='_get_infos_response_data', store=True)
+
+    @api.depends('response_data')
+    def _get_infos_response_data(self):
+        for record in self:
+            response_data = record.response_data
+            length_response_data = len(response_data)
+            summary_response_data = response_data
+            if length_response_data > RESPONSE_DATA_MAX_CHARACTERS:
+                summary_response_data = \
+                    response_data[:RESPONSE_DATA_MAX_CHARACTERS]
+                summary_response_data += '.....'
+            record.length_response_data = length_response_data
+            record.summary_response_data = summary_response_data
+            record.file_response_data = \
+                base64.b64encode(response_data.encode())
+            record.filename_response_data = \
+                'response_data_{}.log'.format(record.id)
 
     def create_log(self, version_id, request_data, response_data, user=False):
         public_user = self.env.ref('base.public_user')
