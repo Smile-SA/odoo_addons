5607cd22d20d84e6fe961b39053842a94bf7cf14
Andrius Laukavičius <andrius@focusate.eu>
2022-04-19 15:58:33 +0300

    [REM] smile_web_auto_refresh: disable feature
    This feature causes connection leaks, so disabling for now.

--- smile_web_auto_refresh/__manifest__.py
+++ smile_web_auto_refresh/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     'name': 'Auto Refresh',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'depends': [
         'web',
         'bus',
@@ -17,7 +17,8 @@
     'category': 'Tools',
     'sequence': 20,
     'data': [
-        'views/webclient_templates.xml',
+        # Disabling functionality to avoid any leaks for now!
+        # 'views/webclient_templates.xml',
     ],
     'qweb': [],
     'auto_install': False,
