569c59fa69f35aceada7be12d83ce7a8f4a83d4c
Isabelle RICHARD <isabelle.richard@smile.fr>
2025-07-07 22:57:55 +0200

    [IMP] smile_audit: define tests less intrusive
    When other modules contains tests based on model res.partner, something is broken.
    Use country, not often overriden, in smile_audit, to avoid collisions with other modules.

--- smile_audit/tests/test_audit.py
+++ smile_audit/tests/test_audit.py
@@ -6,46 +6,57 @@ class TestAudit(TransactionCase):
     def setUp(self):
         super(TestAudit, self).setUp()
         rule_vals = {
-            'name': 'Audit rule on partners',
-            'model_id': self.env.ref('base.model_res_partner').id,
-            'log_create': True
+            "name": "Audit rule on countries",
+            "model_id": self.env.ref("base.model_res_country").id,
+            "log_create": True,
         }
-        self.env['audit.rule'].create(rule_vals)
-        partner_vals = {
-            'name': 'Partner',
-            'is_company': False,
-            'email': 'LasLabs@ExAmPlE.CoM',
+        self.env["audit.rule"].create(rule_vals)
+        country_vals = {
+            "name": "La Terre du milieu",
+            "code": "JRR",
         }
-        self.partner = self.env['res.partner'].create(partner_vals)
+        self.country = self.env["res.country"].create(country_vals)
 
     def test_log_created_on_create(self):
-        """ A log should be created on creating a partner"""
-        log = self.env['audit.log'].search([
-            ('model_id', '=', self.env.ref('base.model_res_partner').id),
-            ('method', '=', 'create'),
-            ('res_id', '=', self.partner.id),
-        ], limit=1)
+        """A log should be created on creating a country"""
+        log = self.env["audit.log"].search(
+            [
+                ("model_id", "=", self.env.ref("base.model_res_country").id),
+                ("method", "=", "create"),
+                ("res_id", "=", self.country.id),
+            ],
+            limit=1,
+        )
         self.assertEqual(
-            log.name, 'Partner', 'No audit log after partner creation')
+            log.name,
+            "La Terre du milieu",
+            "No audit log after country creation",
+        )
 
     def test_log_created_on_write(self):
-        """ A log should be created on updating a partner"""
-        self.partner.write({'name': 'Updated Partner'})
-        log = self.env['audit.log'].search([
-            ('model_id', '=', self.env.ref('base.model_res_partner').id),
-            ('method', '=', 'write'),
-            ('res_id', '=', self.partner.id),
-        ])
+        """A log should be created on updating a country"""
+        self.country.write({"name": "Mordor"})
+        log = self.env["audit.log"].search(
+            [
+                ("model_id", "=", self.env.ref("base.model_res_country").id),
+                ("method", "=", "write"),
+                ("res_id", "=", self.country.id),
+            ]
+        )
         self.assertEqual(
-            log.res_id, self.partner.id, 'No audit log after partner updating')
+            log.res_id, self.country.id, "No audit log after country updating"
+        )
 
     def test_log_created_on_unlink(self):
-        """ A log should be created on deleting a partner"""
-        self.partner.unlink()
-        log = self.env['audit.log'].search([
-            ('model_id', '=', self.env.ref('base.model_res_partner').id),
-            ('method', '=', 'unlink'),
-            ('res_id', '=', self.partner.id),
-        ])
+        """A log should be created on deleting a country"""
+        self.country.unlink()
+        log = self.env["audit.log"].search(
+            [
+                ("model_id", "=", self.env.ref("base.model_res_country").id),
+                ("method", "=", "unlink"),
+                ("res_id", "=", self.country.id),
+            ]
+        )
         self.assertEqual(
-            log.name, 'Partner', 'No audit log after partner unlink')
+            log.name, "La Terre du milieu", "No audit log after country unlink"
+        )
